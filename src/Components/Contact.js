b5c821f07a4284f7571fbeb0a85eb3c778345bdc
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-12-27 15:45:06 +0100

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Fade, Slide } from "react-reveal";
+
+class Contact extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const { name, address, phone, contactmessage, email } = this.props.data;
+    const { street, city, state, zip } = address;
+
+    return (
+      <section id="contact">
+        <Fade bottom duration={1000}>
+          <div className="row section-head">
+            <div className="two columns header-col">
+              <h1>
+                <span>Get In Touch.</span>
+              </h1>
+            </div>
+
+            <div className="ten columns">
+              <p className="lead">{contactmessage}</p>
+            </div>
+          </div>
+        </Fade>
+
+        <div className="row">
+          <Slide left duration={1000}>
+            <div className="eight columns">
+              <p>Click the button below to send me an email directly:</p>
+              <a
+                href={`mailto:${email}?subject=Get In Touch&body=Hi ${name},%0D%0A%0D%0A`}
+                className="submit"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Send Email
+              </a>
+            </div>
+          </Slide>
+
+          <Slide right duration={1000}>
+            <aside className="four columns footer-widgets">
+              <div className="widget widget_contact">
+                <h4>Address and Phone</h4>
+                <p className="address">
+                  {name}
+                  <br />
+                  {street} <br />
+                  {city}, {state} {zip}
+                  <br />
+                  <span>{phone}</span>
+                </p>
+              </div>
+            </aside>
+          </Slide>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact;
